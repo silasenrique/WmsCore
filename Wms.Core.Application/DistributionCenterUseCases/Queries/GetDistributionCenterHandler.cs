ffcfa0431df5575e2a2d9c0ea75f6d5e31c0a3a7
Silas <silas.enriques@gmail.com>
2022-10-12 12:55:46 -0300

    #29 removi totalmente a dependencia do pacote Mapster

--- Wms.Core.Application/DistributionCenterUseCases/Queries/GetDistributionCenterHandler.cs
+++ Wms.Core.Application/DistributionCenterUseCases/Queries/GetDistributionCenterHandler.cs
@@ -1,5 +1,4 @@
 using System.Linq.Expressions;
-using MapsterMapper;
 using Wms.Core.Application.Common.Interfaces.Messaging;
 using Wms.Core.Application.DistributionCenterUseCases.Contract;
 using Wms.Core.Domain.Entities.Entity;
